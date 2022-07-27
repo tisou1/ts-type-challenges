1468db34386217838fa3e255dcf43fba8ad0f7b2
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-07-27 22:17:54 +0800

    chore: add another solution

--- src/05310-medium-join/templates.ts
+++ src/05310-medium-join/templates.ts
@@ -1,7 +1,19 @@
-type Join<T extends unknown[], U extends string | number> = 
-  T extends [infer F, ...infer R]
-    ? R['length'] extends 0
-      ? F
-      : `${F & string}${U}${Join<R,U>}`
-    : ''
+// type Join<T extends unknown[], U extends string | number> = 
+//   T extends [infer F, ...infer R]
+//     ? R['length'] extends 0
+//       ? F
+//       : `${F & string}${U}${Join<R,U>}`
+//     : ''
 
+
+type Join<T extends unknown[], U extends string | number, S extends string = ''> = 
+  T extends [infer F, ...infer R]
+    ? Join<R, U, S extends '' ? F : `${S}${U}${F & string}`>
+    : S
+
+    
+    type gis = Join<[1,2,3,4,5], '+'>
+
+/**
+ * 如果不是string的数组, 可能会有一点问题
+ */
