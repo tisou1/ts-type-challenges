8d5c203dd732914b78bbbf9e6c2bb91f4fc65525
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-07-27 11:53:46 +0800

    chore: add 05117-medium-without

---
+++ src/05117-medium-without/template.ts
@@ -0,0 +1,14 @@
+//主要是转化一下U为基础类型情况.
+type TupleToUnion1<T extends unknown[] | unknown > = T extends unknown[] ? T[number] : T
+
+type Without<T, U, Result extends unknown[] = []> =
+  T extends [infer F, ...infer R]
+    ? F extends TupleToUnion1<U>
+      ? Without<R, U, Result>
+      : Without<R, U, [...Result, F]>
+    : T
+
+
+    type ol3  = Without<[1, 2], 1>
+
+    type ol333 =  Without<[1, 2, 4, 1, 5], [1, 2]>
