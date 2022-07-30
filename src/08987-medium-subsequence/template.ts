6d802d58eef496771031c54cd8c500ff1bf21a02
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-07-30 22:51:19 +0800

    chore: update

--- src/08987-medium-subsequence/template.ts
+++ src/08987-medium-subsequence/template.ts
@@ -3,4 +3,6 @@ type Subsequence<T extends any[]> =
     ? [F, ...Subsequence<R>] | Subsequence<R>
     : []
 
+
+    //Subsequence总是返回当前整个数组和这个数组少一位之后的递归([F, ...Subsequence<R>] | Subsequence<R>)
     type asd2f = Subsequence<[1, 2, 3]>
