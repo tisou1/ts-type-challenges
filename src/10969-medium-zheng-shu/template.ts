0507794a1db39ef565c60f27887a6fc6031133cb
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-11-09 22:32:58 +0800

    chore: 10969 new anwser

--- src/10969-medium-zheng-shu/template.ts
+++ src/10969-medium-zheng-shu/template.ts
@@ -1,6 +1,14 @@
-type Integer<T> = 
-  `${T & number}` extends `${number}.${number}`
-    ? never
-    : 0.1 extends T
-      ? never
-      : T
+// type Integer<T> = 
+//   `${T & number}` extends `${number}.${number}`
+//     ? never
+//     : 0.1 extends T
+//       ? never
+//       : T
+
+// type Integer<T extends number> =
+//   number extends T 
+//     ? never
+//     : `${T}` extends `${string}.${string}` ? never : T
+
+type Integer<T extends number> = `${T}` extends `${bigint}` ? T : never
+
